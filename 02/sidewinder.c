0e88e817fa2aa1783eacfe184b6ce36d904abe77
troi <blametroi@gmail.com>
2024-09-07 11:21:56 -0400

    added a rather poor description of sidewinder algorithm

--- 02/sidewinder.c
+++ 02/sidewinder.c
@@ -3,6 +3,22 @@
 /*
  * walk through all the cells in the passed grid and link neighboring
  * cells using the sidewinder algorithm.
+ *
+ * sidewinder traverses the grid starting at the south western corner
+ * of the grid, linking horizontal runs of cells moving eastward (50%
+ * chance) until either the eastern boundary is reached or a move
+ * northward is rolled (50% chance).
+ *
+ * this closes the run. once cell in the run is picked at random and
+ * linked to its northern neighbor. while this cell is linked, it is
+ * not considered to have been visited.
+ *
+ * sidewinder advances to the next cell to the east of the end of the
+ * run unless it is at the boundary, then it moves to the western most
+ * cell of the next row to the north.
+ *
+ * this continues until the north eastern cornder of the grid is
+ * reached.
  */
 
 #include <stdbool.h>
@@ -15,9 +31,9 @@
 grid *
 sidewinder_on(grid *grid) {
 	ASSERT_GRID(grid);
-	cell *run[grid->rows * grid->cols + 1];
+	cell **run = malloc((grid->rows * grid->cols + 1) * sizeof(cell));
 	for (int row = 0; row < grid->rows; row++) {
-		memset(run, 0, sizeof(run));
+		memset(run, 0, (grid->rows * grid->cols + 1) * sizeof(cell));
 		int cells = 0;
 		for (int col = 0; col < grid->cols; col++) {
 			cell *curr = cell_in_grid_at(grid, row, col);
@@ -31,12 +47,13 @@ sidewinder_on(grid *grid) {
 			if (should_close_out) {
 				cell *member = run[random_between(0, cells-1)];
 				link_cell(member, member->north, true);
-				memset(run, 0, sizeof(run));
+				memset(run, 0, (grid->rows * grid->cols + 1) * sizeof(cell));
 				cells = 0;
 			} else
 				link_cell(curr, curr->east, true);
 		}
 	}
+	free(run);
 	return grid;
 }
 
